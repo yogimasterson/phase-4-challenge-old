1ee9032b7dd7fa98ca5ed07834bc8b4ac2c965e6
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-10-03 16:20:42 -0700

    handled error messages, created nav bar, header, footer

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser')
+const routes = require('./server/routes')
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/', routes)
+
+app.use((req, res) => {
+  res.status(404).render('common/not_found')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}...`)
+})
